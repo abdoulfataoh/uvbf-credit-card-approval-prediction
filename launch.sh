0c23e3ebfa349816c1a22008ae2197343faf8a83
abdoulfataoh <abdoulfataoh@gmail.com>
2022-01-25 11:44:21 +0000

    update launch.sh

--- launch.sh
+++ launch.sh
@@ -5,14 +5,23 @@ echo "enter 1 for logistic-regression"
 echo "enter 2 for random-forest "
 echo -n "your answer: "
 read
-echo "" 
+
+# choose tag
 case $REPLY in
 1) echo "switching on logistic-regression model"
-   git checkout logistic-regression-experiment ;;
+   git checkout logistic-regression-experiment
+   TAG="logistic-regression-experiement" ;;
 2) echo "switching on random-forest model"
-   git checkout random-forest-experiment ;;
+   git checkout random-forest-experiment
+   TAG="random-forest-experiment" ;;
 *) echo "exited"; exit
 esac
+
+# clear screen
+clear
+
+# verbose
+echo "switched on $TAG ....done"
 
 # google drive info
 echo -n "enter google drive id for data backup: "
